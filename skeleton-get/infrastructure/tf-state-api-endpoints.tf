057254facabe8b6081ce7579e28b0ba012101a39
James Hall <james.hall@azcard.io>
2018-01-27 18:19:05 +0000

    [DEV-0128] - Fix remote state key for api endpoints

--- skeleton-get/infrastructure/tf-state-api-endpoints.tf
+++ skeleton-get/infrastructure/tf-state-api-endpoints.tf
@@ -5,7 +5,7 @@ data "terraform_remote_state" "api_endpoints" {
 
   config {
     bucket = "tf-state-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"
-    key    = "${var.project}/${data.aws_caller_identity.current.account_id}/${data.aws_region.current.name}/one-time/api-endpoints.tfstate"
+    key    = "${var.project}/${data.aws_caller_identity.current.account_id}/${data.aws_region.current.name}/${var.environment}/api-endpoints.tfstate"
     region = "${data.aws_region.current.name}"
   }
 }
