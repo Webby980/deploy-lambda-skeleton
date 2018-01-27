7162b038501d42935a9632af942742258064b299
James Hall <james.hall@azcard.io>
2018-01-27 18:26:49 +0000

    [DEV-0128] - Fix output usage for get lambda in alias component

--- aliases/infrastructure/aliases.tf
+++ aliases/infrastructure/aliases.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_alias" "get" {
   name             = "${var.environment}"
   description      = "The alias for create_iak for the ${var.environment} environment, built from ${var.git_hash}"
-  function_name    = "${data.terraform_remote_state.get.get_arn}"
-  function_version = "${data.terraform_remote_state.get.get_version}"
+  function_name    = "${data.terraform_remote_state.get.arn}"
+  function_version = "${data.terraform_remote_state.get.version}"
 }
