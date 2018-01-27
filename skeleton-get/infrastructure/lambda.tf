ffb9abf3db04d9ef54e4d0c54a1b3184303bd69e
James Hall <james.hall@azcard.io>
2018-01-27 18:23:34 +0000

    [DEV-0128] - Add path part to fetch of lambda source in s3

--- skeleton-get/infrastructure/lambda.tf
+++ skeleton-get/infrastructure/lambda.tf
@@ -5,7 +5,7 @@ resource "aws_lambda_function" "lambda" {
     role = "${data.terraform_remote_state.api_lambda_role.api_lambda_role_arn}"
 
     s3_bucket = "${data.terraform_remote_state.lambda_artifacts_s3_bucket.lambda_artifacts_s3_bucket_id}"
-    s3_key = "${var.lambda_name}/${var.git_hash}.zip"
+    s3_key = "${var.path_part}/${var.lambda_name}/${var.git_hash}.zip"
     source_code_hash = "${data.aws_s3_bucket_object.s3_source_hash.body}"
 
     publish = true
