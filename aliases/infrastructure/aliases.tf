9bb67f1fa5c8d596daa701508d38075dba270552
matthew.puzey <matthew.puzey@nhs.net>
2018-01-28 21:40:22 +0000

    [DEV-0128] - Fix description

--- aliases/infrastructure/aliases.tf
+++ aliases/infrastructure/aliases.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_alias" "get" {
   name             = "${var.environment}"
-  description      = "The alias for create_iak for the ${var.environment} environment, built from ${var.git_hash}"
+  description      = "The alias for the get method for ${var.path_part} in the ${var.environment} environment, built from ${var.git_hash}"
   function_name    = "${data.terraform_remote_state.get.arn}"
   function_version = "${data.terraform_remote_state.get.version}"
 }
