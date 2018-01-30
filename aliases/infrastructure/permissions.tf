824a3b0124346b593a17365140329e81f51e100f
matthew.puzey <matthew.puzey@nhs.net>
2018-01-30 20:10:24 +0000

    [DEV-0128] - Fix endpoints output usage

--- aliases/infrastructure/permissions.tf
+++ aliases/infrastructure/permissions.tf
@@ -3,6 +3,6 @@ resource "aws_lambda_permission" "get" {
   action        = "lambda:InvokeFunction"
   function_name = "${data.terraform_remote_state.get.arn}"
   principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${data.terraform_remote_state.api_gateway.api_gateway_id}/*/GET${data.terraform_remote_state.api_endpoints.request_iak_endpoint_resource_path}"
+  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${data.terraform_remote_state.api_gateway.api_gateway_id}/*/GET${data.terraform_remote_state.api_endpoints.resource_path}"
   qualifier = "${aws_lambda_alias.get.name}"
 }
