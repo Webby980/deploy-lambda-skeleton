1840c195a89293c8138d2e2d4eae1afb0243e3a8
Alistair Webster <alistair.webster@nhs.net>
2018-01-30 21:12:14 +0000

    [DEV-0128]- Edit proxy integration to be a POST rather than GET

--- skeleton-get/infrastructure/integration.tf
+++ skeleton-get/infrastructure/integration.tf
@@ -5,7 +5,7 @@ resource "aws_api_gateway_integration" "integration" {
   rest_api_id = "${data.terraform_remote_state.api_gateway.api_gateway_id}"
   resource_id = "${data.terraform_remote_state.api_endpoints.resource_id}"
   http_method = "${aws_api_gateway_method.resource_method.http_method}"
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${aws_lambda_function.lambda.arn}:$${stageVariables.lambdaAlias}/invocations"
 }
